e6327631a0f2537d15588bb457bafca9ac960d54
IAskOfYou <iaskofyou77@gmail.com>
2024-11-14 08:21:45 -0500

    commit

--- Card.cs
+++ Card.cs
@@ -1,8 +1,11 @@
 namespace Game {
     public class Card {
+        //Enum types of the card
+        private enum EnumType {Card, Servant}
         public bool Visible {get; set;}
         public string Name {get; set;}
         public string Type {get; set;}
+        private EnumType internalType;
         public string Body {get; set;}
         public string Faction {get; set;} = "none";
         public int Health {get; set;} = 1;
@@ -31,5 +34,13 @@ namespace Game {
             Console.WriteLine("Owie!");
             Health -= damageCount;
         }
+        //This function handles the presenting of the card to the playspace, but effects
+        //are handled elsewhere
+        public void PlayCard(List<Card> cardsInPlay) {
+        }
+    }
+    //Servants have a few extra rules to normal cards. See rulebook.
+    public class Servant : Card {
+
     }
 }
